d59300987f3764294f1753ca3465df72943b7e41
Natalia_Melnikova <melnikova.n@compmechlab.ru>
2023-11-01 22:19:31 +0300

    HomeWork 3

---
+++ hw_task_3/Test3.py
@@ -0,0 +1,75 @@
+# задание 1 - ok
+
+# class rectangle:
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+#
+#     def perimeter(self):
+#         print(2 * self.length + 2 * self.width)
+#
+#     def area(self):
+#         print(self.length * self.width)
+#
+# w = rectangle(5, 2)
+# w.perimeter()
+# w.area()
+
+
+
+# задание 2 - ne ok
+
+# class Teacher:
+#     Final_students = 5
+#     # def __init__(self, name):
+#     #     self.name = name
+#
+#     def teach(self, Object_name, number_of_students):
+#         self.name = Object_name
+#         self.number = number_of_students
+#         self.Final_students = self.Final_students + 1
+#
+#         print(self.Final_students)
+#         print(Object_name, number_of_students)
+#
+# class Student:
+#     knowledge =
+
+# class Materials:
+#
+#     def __new__(cls, *args, **kwargs):
+#         return super().__new__(cls)
+#
+#     def __init__(self, attr, args=None):
+#         self.attr = args
+#         print(self.attr)
+#
+# obj = Materials()
+# obj.init(5)
+
+
+# print(obj.teach)
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def take(self, name):
+#         self.name = Teacher(self.name)
+#
+# stud = Student('math')
+# print(stud.take)
+
+
+# задание 3 - ne ok
+
+# class apple:
+#     stadiya = ["no", "green", "red"]
+#
+#     # def __init__(self, state):
+#     #     self.state = state
+#
+#     def print(self):
+#         print(self.stadiya[1])
+#
+# w = apple()
+# w.print()
