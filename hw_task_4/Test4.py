8a4b0b190b05f4446f9618e704163d61a7c3ae67
Natalia_Melnikova <melnikova.n@compmechlab.ru>
2023-11-01 22:19:48 +0300

    HomeWork 4

---
+++ hw_task_4/Test4.py
@@ -0,0 +1,50 @@
+# #задание 1 - ok
+# import numpy as np
+# a = np.random.randint(50, size=10, dtype=int)
+# ## print(a)
+# my_list = sorted(list(filter(lambda x: x > 15, a)))
+# print(my_list)
+
+
+#задание 2 - ok
+# import numpy as np
+# a = 1
+# b = 0
+# c = 4
+# str1 = np.zeros(8)
+# str2 = np.ones(8)
+#
+# str1[1::2] = a
+# str2[1::2] = b
+#
+# # print(str1)
+# # print(str2)
+#
+# gg = np.array([str1, str2], int)
+# # print(gg)
+# gg_new = np.append(gg, np.array([str1, str2]), axis=0)
+# # print(gg_new)
+# ggg_new = np.append(gg_new, np.array(gg_new), axis=0)
+# print(ggg_new)
+#
+# print(ggg_new.shape)
+
+#задание 3 - ok
+# import numpy as np
+# a = np.random.randint(5, size=(8, 4))
+# b = np.random.randint(5, size=(4, 2))
+# print(a)
+# print(b)
+#
+# c = np.dot(a, b)
+# print(c)
+
+
+#задание 4 - ok
+# import numpy as np
+# a = np.linspace(0, 1, 10)[1:-1]
+# print(a)
+
+
+#задание 5
+import numpy as np
